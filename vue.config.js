1fcb30db9d95025948dec6b94ad061d96633edd6
Stephenjoel2k <stephenjoel2k@hotmail.com>
2021-04-01 17:43:04 -0500

    Updated vue configs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+const {GenerateSW} = require('workbox-webpack-plugin');
+
 module.exports = {
+  pwa: {
+    name: 'MusicHabits',
+    themeColor: '#000',
+    msTileColor: '#000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+        swDest: 'sw.js',
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
